38f56c88d4ffa712127fb575661bcccb7c6f67a1
Bogomil Roussev <bogomil@thecloudsolutions.com>
2025-05-20 11:12:53 +0300

    delete nextjs project

--- app/blog/page.js
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function BlogRedirect() {
-  redirect('https://app.gitbook.com/o/oEtxqQppLRLq2RNJ5VkB/s/YAWUsG4dXhPUXBViZgXQ/')
-}
--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <section className="text-center p-16 bg-gray-50">
-      <h1 className="text-5xl font-extrabold text-primary mb-4">Hi, I'm Bogomil Roussev</h1>
-      <p className="text-lg text-gray-700 mb-8">Security Analyst · DevOps Enthusiast · Continuous Learner</p>
-      <a href="/about" className="inline-block bg-primary text-white py-2 px-6 rounded-full text-lg shadow hover:opacity-90 transition">Learn More</a>
-    </section>
-  )
-}
